71b249b306c93274310d9a08073353518f242b75
taxio <taxio.2029@gmail.com>
2018-06-08 18:52:23 +0900

    簡単なWebサーバを構築

--- program/digit_act/digit_act.ino
+++ program/digit_act/digit_act.ino
@@ -1,7 +1,15 @@
+#include "SimpleWebServer.h"
+#include <WiFi.h>
+#include "setting.h"
+
 #define MOTOR0 25
 #define MOTOR1 26
 #define LED 27
 #define BUTTON 16
+
+IPAddress ip(192, 168, 42, 2);
+IPAddress gateway(192, 168, 42, 1);
+IPAddress subnet(255, 255, 255, 0);
 
 /*
  * digitalWrite(motor0,HIGH/LOW);
@@ -9,6 +17,47 @@
  * これでmotorのon/offができます
  * 
  */
+
+SimpleWebServer server(
+  APSSID, APPASS, 
+  IPAddress(192,168,42,2),  // ip
+  IPAddress(192,168,42,1),  // gateway
+  IPAddress(255,255,255,0), 80); // subnet, port
+
+void menu() {
+  const char* cur = digitalRead(LED) ? "ON" : "OFF";
+  String s =  "HTTP/1.1 200 OK\r\nContent-type:text/html\r\n\r\n"
+              "<HTML><BODY style='font-size:48px;'>ESP32_HTTPD_LED1<br/>"
+              "<br/><a href=/ledon>ON</a><br/><a href=/ledoff>OFF</a><br/><br/>"
+              "Current Status = ";
+  s = s + String(cur) + "</BODY></HTML>\r\n";
+  server.send_response(s.c_str());
+}
+
+bool flgDoor = false;
+void doorOn(){
+  flgDoor = true;
+  Serial.println("door on");
+}
+void doorOff(){
+  flgDoor = false;
+}
+
+bool flgLab = false;
+void labOn(){
+  flgLab = true;
+  Serial.println("lab on");
+}
+void labOff(){
+  flgLab = false;
+}
+
+void defaultHandler(String requestLine){
+  Serial.println("default_handler : " + requestLine);  
+  String s = requestLine + " is not found";
+  server.send_status(404, s.c_str());
+}
+
 
 void runMotor0(int runTime){
   digitalWrite(MOTOR0, HIGH);
@@ -21,6 +70,20 @@ void runMotor1(int runTime){
   digitalWrite(MOTOR1, LOW);
 }
 
+void ledOn(){
+  digitalWrite(LED, 1);
+}
+void ledOff(){
+  digitalWrite(LED, 0);
+}
+void ledToggle(){
+  if(digitalRead(LED)){
+    ledOff();
+  }else{
+    ledOn();
+  }
+}
+
 void setup() {
 
   // pin mode setup
@@ -31,15 +94,36 @@ void setup() {
 
   // serial setup
   Serial.begin(115200);
+  delay(100);
+  Serial.println("this is actuator program");
+
+  // WiFi setup
+  WiFi.config(ip, gateway, subnet);
+  delay(100);
+  WiFi.begin(APSSID, APPASS);
+  Serial.println("WiFi connecting");
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    ledToggle();
+    delay(500);
+  }
+  Serial.println("\nconnencted");
+  ledOff();
+  Serial.print("Module IP : ");
+  Serial.println(WiFi.localIP());
+
+  // server setup
+  server.add_handler("/door", doorOn);
+  server.add_handler("/lab", labOn);
+  server.add_default_handler(defaultHandler);
+  
+  server.begin();
+  Serial.println("server start");
 }
 
-
-
 void loop() {
-
-  Serial.println("hey!");
-  delay(1000);
-
+  server.handle_request();
+  delay(10);
 }
 
 
